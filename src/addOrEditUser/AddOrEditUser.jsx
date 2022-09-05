c4b3f3c38db706724a1b8cf621b356537fffd641
Your Name <miladraouf73@gmail.com>
2022-09-05 16:36:34 +0430

    implementation of add user

--- src/addOrEditUser/AddOrEditUser.jsx
+++ src/addOrEditUser/AddOrEditUser.jsx
@@ -1,8 +1,37 @@
+import axios from "axios";
+import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const AddOrEditUser = () => {
     const { userId } = useParams();
     const navigate = useNavigate();
+
+    const [userDetails, setUserDetails] = useState(
+        {
+            "name": "",
+            "username": "",
+            "email": "",
+            "address": {
+                "street": "",
+                "suite": "",
+                "city": "",
+                "zipcode": ""
+            }
+        }
+    );
+
+    const handleAddUser = (e) => {
+        e.preventDefault();
+
+        axios.post('https://jsonplaceholder.typicode.com/users', userDetails).then((res) => {
+            Swal.fire(
+                userDetails.name,
+                'تبریک میگم به لیست کاربران اضافه شدی',
+                'success'
+            )
+        })
+    };
 
     return (
         <section className="container form-section">
@@ -13,26 +42,26 @@ const AddOrEditUser = () => {
                             {userId ? 'ویرایش' : 'افزودن'} کاربر
                         </h4>
 
-                        <form>
+                        <form onSubmit={handleAddUser}>
                             <article className="name">
                                 <label>
                                     نام و نام خانوادگی
                                 </label>
-                                <input type="text" id="name_input"></input>
+                                <input type="text" id="name_input" onChange={(e) => { setUserDetails({ ...userDetails, name: e.target.value }) }}></input>
                             </article>
 
                             <article className="user-name">
                                 <label>
                                     نام کاربری
                                 </label>
-                                <input type="text" id="username_input"></input>
+                                <input type="text" id="username_input" onChange={(e) => { setUserDetails({ ...userDetails, username: e.target.value }) }}></input>
                             </article>
 
                             <article className="email">
                                 <label>
                                     ایمیل
                                 </label>
-                                <input type="text" id="email_input"></input>
+                                <input type="text" id="email_input" onChange={(e) => { setUserDetails({ ...userDetails, email: e.target.value }) }}></input>
                             </article>
 
                             <article>
