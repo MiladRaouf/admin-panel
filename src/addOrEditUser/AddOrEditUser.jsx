109dfaa7469c36163cc0858f533b693cf05c1904
Your Name <miladraouf73@gmail.com>
2022-09-05 17:11:00 +0430

    implementation of edit user

--- src/addOrEditUser/AddOrEditUser.jsx
+++ src/addOrEditUser/AddOrEditUser.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Swal from "sweetalert2";
 
@@ -7,24 +7,38 @@ const AddOrEditUser = () => {
     const { userId } = useParams();
     const navigate = useNavigate();
 
-    const [userDetails, setUserDetails] = useState(
-        {
-            "name": "",
-            "username": "",
-            "email": "",
-            "address": {
-                "street": "",
-                "suite": "",
-                "city": "",
-                "zipcode": ""
-            }
+    const [userDetails, setUserDetails] = useState({
+        "name": "",
+        "username": "",
+        "email": "",
+        "address": {
+            "street": "",
+            "suite": "",
+            "city": "",
+            "zipcode": ""
         }
-    );
+    });
+
+    useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`).then(res => {
+            setUserDetails({
+                "name": res.data.name,
+                "username": res.data.username,
+                "email": res.data.email,
+                "address": {
+                    "street": "",
+                    "suite": "",
+                    "city": "",
+                    "zipcode": ""
+                }
+            });
+        });
+    }, []);
 
     const handleAddUser = (e) => {
         e.preventDefault();
 
-        axios.post('https://jsonplaceholder.typicode.com/users', userDetails).then((res) => {
+        axios.post('https://jsonplaceholder.typicode.com/users', userDetails).then(() => {
             Swal.fire(
                 userDetails.name,
                 'تبریک میگم به لیست کاربران اضافه شدی',
@@ -32,6 +46,19 @@ const AddOrEditUser = () => {
             )
         })
     };
+
+    const handleEditUser = (e) => {
+        e.preventDefault();
+
+        axios.put(`https://jsonplaceholder.typicode.com/users/${userId}`, userDetails).then((res) => {
+            Swal.fire(
+                userDetails.name,
+                'تغییرات مورد نظرت انجام شد',
+                'success'
+            )
+        })
+
+    }
 
     return (
         <section className="container form-section">
@@ -42,26 +69,26 @@ const AddOrEditUser = () => {
                             {userId ? 'ویرایش' : 'افزودن'} کاربر
                         </h4>
 
-                        <form onSubmit={handleAddUser}>
+                        <form onSubmit={userId ? handleEditUser : handleAddUser}>
                             <article className="name">
                                 <label>
                                     نام و نام خانوادگی
                                 </label>
-                                <input type="text" id="name_input" onChange={(e) => { setUserDetails({ ...userDetails, name: e.target.value }) }}></input>
+                                <input type="text" id="name_input" value={userDetails.name} onChange={(e) => { setUserDetails({ ...userDetails, name: e.target.value }) }}></input>
                             </article>
 
                             <article className="user-name">
                                 <label>
                                     نام کاربری
                                 </label>
-                                <input type="text" id="username_input" onChange={(e) => { setUserDetails({ ...userDetails, username: e.target.value }) }}></input>
+                                <input type="text" id="username_input" value={userDetails.username} onChange={(e) => { setUserDetails({ ...userDetails, username: e.target.value }) }}></input>
                             </article>
 
                             <article className="email">
                                 <label>
                                     ایمیل
                                 </label>
-                                <input type="text" id="email_input" onChange={(e) => { setUserDetails({ ...userDetails, email: e.target.value }) }}></input>
+                                <input type="text" id="email_input" value={userDetails.email} onChange={(e) => { setUserDetails({ ...userDetails, email: e.target.value }) }}></input>
                             </article>
 
                             <article>
